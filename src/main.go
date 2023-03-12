910905adf25ebbc7f369ad6e9e81b133c4751a0a
Cezar Fuhr <cezar@nextar.com.br>
2023-03-12 10:21:19 -0300

    version9

--- src/main.go
+++ src/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// rota que responde a requisições GET na raiz "/"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World version 8!")
+		fmt.Fprintf(w, "Hello World version 9!")
 	})
 
 	// inicia o servidor na porta 8080
